424a05fa670fa6f8c3dba0406917be9183c6d46c
Denise Trocchi <denise.trocchi@yahoo.it>
2022-10-29 11:35:43 +0200

    last minute updates (part 2)

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ console.clear()
 
 let check = document.querySelector(".check"),
     form = check.querySelector(".check form"),
-    input = form.querySelector(".check input");
+    input = form.querySelector("input"),
+    caption = form.querySelector("small");
 
 function activate(x){
   x.classList.add("active");
@@ -30,7 +31,18 @@ function init(){
 
 input.addEventListener("input", (event) => {
   let val = input.value.toLowerCase();
-  if (val == "beatrice") {
-    init()
+  if(val.slice(0,1) == "b") {
+    caption.innerHTML = "Daje, avanti così <span>&#9997;</span>"
+  } else {
+    caption.innerHTML = "Ma l'hai letto l'indizietto?! <span>&#128530;</span>"
+  }
+  if(val.slice(0,5) == "beatr") {
+    caption.innerHTML = "<strong>No pressure</strong>, non ti stiamo osservando <span>&#128064;</span>"
+  }
+  if(val.slice(0,8) == "beatrice") {
+    caption.innerHTML = "GRANDEEE <span>&#127881;</span>"
+    setTimeout(() => {
+      init()
+    }, 1000)
   }
 });
